53fd6742dfbfed4690271bb8c61f95167f0cc2c0
Fahim1272 <imfahim72@gmail.com>
2022-11-10 19:46:16 +0600

    delete button updated

--- src/Pages/MyReviews/MyReviews.js
+++ src/Pages/MyReviews/MyReviews.js
@@ -1,17 +1,40 @@
 import React, { useContext, useEffect, useState } from 'react';
 import { AuthContext } from '../../Contexts/AuthProvider';
+import ReviewRow from './ReviewRow';
 
 const MyReviews = () => {
-    const {user} = useContext(AuthContext)
-    const [reviews, setReviews ] = useState({})
-    useEffect(()=>{
+    const { user } = useContext(AuthContext)
+    const [reviews, setReviews] = useState([])
+    useEffect(() => {
         fetch(`http://localhost:5000/reviews?email=${user.email}`)
-        .then(res => res.json())
-        .then(data =>setReviews(data))
-    },[user?.email])
+            .then(res => res.json())
+            .then(data => setReviews(data))
+    }, [user?.email])
     return (
         <div>
             <h2>My Reviews {reviews.length}</h2>
+            <div className="overflow-x-auto w-full">
+                <table className="table w-full">
+                    <thead>
+                        <tr>
+                            <th>
+                                <label>
+                                    <input type="checkbox" className="checkbox" />
+                                </label>
+                            </th>
+                            <th>Name</th>
+                            <th>Job</th>
+                            <th>Favorite Color</th>
+                            <th></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            reviews.map(review =><ReviewRow key={review._id} review={review}></ReviewRow>)
+                        }
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
